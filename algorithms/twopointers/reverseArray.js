e3a72895dc6967baf8500af3d6ce834ded99740a
Prabadhya Upadhyay <Prabs07>
2025-05-19 22:07:13 +0530

    Feat:- two pointers reverse Array

---
+++ algorithms/twopointers/reverseArray.js
@@ -0,0 +1,18 @@
+// Given a sorted Array reverse it using different method.(in Place)
+
+function reverseArray(array=[]) {
+    // Solving using two pointers.
+    let lastElementIndex = array.length - 1;
+    let firstElementIndex = 0;
+    while(lastElementIndex>firstElementIndex) {
+        let lastElement = array[lastElementIndex];
+        let firstElement = array[firstElementIndex];
+        array[firstElementIndex] = lastElement;
+        array[lastElementIndex] = firstElement
+        lastElementIndex--
+        firstElementIndex++
+    }
+    console.log(array);
+}
+
+reverseArray([10,20,30,40,50,60,70,80])
