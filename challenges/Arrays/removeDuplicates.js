5656c3783fb290998304b178a5bf5d7d083f068a
Prabadhya Upadhyay <Prabs07>
2025-05-22 19:44:37 +0530

    Feat:- remove duplicates

---
+++ challenges/Arrays/removeDuplicates.js
@@ -0,0 +1,18 @@
+// Remove duplicates in place from sorted Array
+
+function removeDuplicates (nums=[]) {
+    if(nums.length === 0) return 0
+
+    let k = 0
+
+    for(let i = 1 ; i < nums?.length ; i++) {
+        if(nums[k] !== nums[i]) {
+            k++
+            nums[k] = nums[i];
+        }
+    }
+    console.log(nums)
+    return k + 1
+}
+const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+console.log(removeDuplicates(nums))
